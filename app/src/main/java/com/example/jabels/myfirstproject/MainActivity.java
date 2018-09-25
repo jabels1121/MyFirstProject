4d8b9e14c525eed95ec6919503cd9b3d7fbe59e7
Jabels <jabels1121@gmail.com>
2018-09-25 17:37:29 +0300

    Урок 4. Обработчик событий на примере Button

--- app/src/main/java/com/example/jabels/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/jabels/myfirstproject/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.jabels.myfirstproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView textView;
+    private Button helloButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        helloButton = (Button) findViewById(R.id.button2);
+
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Zdarova!");
+            }
+        });
+    }
+
+    public void sayHello(View view) {
+        textView.setText("Hello!");
     }
 }
